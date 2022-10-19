f741e87326ce2840c4ae560036d874e4b3fd6d45
kbernard <k.bernard@belizetelemedia.net>
2022-10-19 11:26:52 -0600

    first commit

--- src/Week1/Lab1/Heading.js
+++ src/Week1/Lab1/Heading.js
@@ -3,9 +3,12 @@ import React from "react"
 function Heading () {
 
     return (
-        <h1>
-            This is a h1 heading
-        </h1>
+        <div>
+            This is the starting code for "Your firs component" ungraded lab
+            <h1>
+                This is a h1 heading
+            </h1>
+        </div>
     )
 }
 
