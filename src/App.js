5dfb4a50f776ec0e79b2e76c0ffcef7d7b4e3977
kbernard <k.bernard@belizetelemedia.net>
2022-10-19 11:40:23 -0600

    completed week 1 labs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import Heading from "./Week1/Lab1/Heading";
+import Heading2 from "./Week1/Lab2/Heading2";
+import Card from "./Week1/Lab3/Card";
 
 function App() {
   return (
     <div className="App">
-      <Heading />
+{/*        <Heading />
+        <Heading2 firstName="Bob"/>
+        <Heading2 firstName="Sandra"/>*/}
+        <Card h2="First card's H2" h3="First card's H3"/>
+        <Card h2="Second card's H2" h3="Second card's H3"/>
+        <Card h2="Third card's H2" h3="Third card's H3"/>
     </div>
   );
 }
---
+++ src/Week1/Lab2/Heading2.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Heading2 (props) {
+
+    return(
+        <h1>Hello, {props.firstName}</h1>
+    )
+}
+
+export default Heading2;
