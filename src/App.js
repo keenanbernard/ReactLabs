0f79092d894b154d0bc5664b21d6bd7e41ddba9f
kbernard <k.bernard@belizetelemedia.net>
2022-10-26 06:33:13 -0600

    completed week 1 labs

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
 {/*        <Heading />
         <Heading2 firstName="Bob"/>
         <Heading2 firstName="Sandra"/>*/}
-        <Card h2="First card's H2" h3="First card's H3"/>
+{/*        <Card h2="First card's H2" h3="First card's H3"/>
         <Card h2="Second card's H2" h3="Second card's H3"/>
-        <Card h2="Third card's H2" h3="Third card's H3"/>
+        <Card h2="Third card's H2" h3="Third card's H3"/>*/}
     </div>
   );
 }
