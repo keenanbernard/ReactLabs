b5bf4671af4fda314f5d297e42fce545aa036009
kbernard <k.bernard@belizetelemedia.net>
2022-10-27 11:25:52 -0600

    Completed calculator app

--- src/App.js
+++ src/App.js
@@ -4,17 +4,12 @@ import Heading from "./Week1/Lab1/Heading";
 import Heading2 from "./Week1/Lab2/Heading2";
 import Card from "./Week1/Lab3/Card";
 import MyVideo from "./Week3/Lab4/MediaPackages";
+import Calculator from "./CalculatorApp/Calculator";
 
 function App() {
   return (
     <div className="App">
-{/*        <Heading />
-        <Heading2 firstName="Bob"/>
-        <Heading2 firstName="Sandra"/>*/}
-{/*        <Card h2="First card's H2" h3="First card's H3"/>
-        <Card h2="Second card's H2" h3="Second card's H3"/>
-        <Card h2="Third card's H2" h3="Third card's H3"/>*/}
-      <MyVideo />
+      <Calculator />
     </div>
   );
 }
